e63ca49c116bb7033aa1c6b4d5999663fcbff533
Nathaniel Low <low.nathaniel@berkeley.edu>
2020-11-09 00:24:17 -0800

    Change PlayerMovement to InputMove

--- Assets/Scripts/Movement/BasicMove.cs
+++ Assets/Scripts/Movement/BasicMove.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
 
-// Moves object in a single direction. Great for testing movement.
 namespace Movement
 {
+    /**
+     * Behaviour that moves an object in a single direction. Great for testing movement.
+     */
     public class BasicMove : MonoBehaviour
     {
         public Vector3 moveDirection = new Vector3(0, 1, 0);
+        
         public Vector3 velocity = new Vector3(1, 1, 1);
 
         // Update is called once per frame
