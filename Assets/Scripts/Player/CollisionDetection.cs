97d81f636d12197c626e9e672c7faf8a95d08d00
NathanielLow <NathanielLow@users.noreply.github.com>
2020-11-10 01:48:14 -0800

    Implement collision detection and bullet grazing (#9)
    * Implement collision detection and bullet grazing
    
    * Add player prefab
    
    * Add Scripts README

---
+++ Assets/Scripts/Player/CollisionDetection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+/*
+ * TODO This can probably be removed and added to some sort of player object when we have that.
+ */
+namespace Player
+{
+    public class CollisionDetection : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.layer == 10)
+            {
+                Debug.Log("I'm hit!");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Shield.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace Player
+{
+    public class Shield : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.layer == 10)
+            {
+                Debug.Log("Shields holding!");
+            }
+        }
+    }
+}
