3a8564e2cd77ff095783b7edaf004ff6b4f9c46e
Andrew Hild <1934043+hirudan@users.noreply.github.com>
2020-11-10 21:10:20 -0800

    Semicircle and Archimedean Spiral patterns (#12)
    * Implement semicircle pattern
    
    * Implement Archimedean spiral pattern
    
    * Correct merge conflicts and add prefabs for new patterns
    
    * Address PR comments
    
    * Amend error in Spiral pattern
    
    * Remove commented code

---
+++ Assets/Scripts/BulletManagement/Patterns/SemiCircle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace BulletManagement.Patterns
+{
+    /*
+     * Emits concentric rings of bullets that subtend a specified angle
+     */
+    public class SemiCircle : BulletPattern
+    {
+        // The angle that subtends the bullet arc
+        public float arcAngle = 150f;
+        // The angle to rotate the pattern (e.g. 270 = straight down)
+        public float offsetAngle = 270f;
+        // The number of bullets per arc
+        public int density = 5;
+        public override Vector3 GetTranslation(double d, int bulletId)
+        {
+            float c = Mathf.Deg2Rad * (arcAngle / (density + 1) * (bulletId + 1) + offsetAngle);
+            var dx = Math.Cos(c);
+            var dy = Math.Sin(c);
+            return new Vector3((float)dx, (float)dy, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/BulletManagement/Patterns/Spiral.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace BulletManagement.Patterns
+{
+    public class Spiral : BulletPattern
+    {
+
+        // Quantity in degrees. Controls how far apart each bullet is
+        public int angleBetweenSpokes = 30;
+
+        // Quantity in degrees. Controls how tight the spiral is.
+        public int spin = 20;
+        
+        public override Vector3 GetInitialPosition(int bulletId)
+        {
+            float c = Mathf.Deg2Rad * angleBetweenSpokes * (bulletId+1);
+            return new Vector3(Mathf.Cos(c), Mathf.Sin(c), 0);
+        }
+        
+        // Roughly traces an Archimedean spiral
+        public override Vector3 GetTranslation(double time, int bulletId)
+        {
+            var dTheta = Mathf.Deg2Rad * spin;
+            var a = dTheta * time;
+            var dx = Math.Cos(a) - a * Math.Sin(a);
+            var dy = Math.Sin(a) + a * Math.Cos(a);
+            
+            return new Vector3((float)dx, (float)dy, 0);
+        }
+    }
+}
