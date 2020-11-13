4cf84ceefcb8efcb087dc3a3610bb644057c2590
Nathaniel Low <low.nathaniel@berkeley.edu>
2020-11-12 23:55:46 -0800

    Add charges to slip time that displays in UI

---
+++ Assets/Scripts/UI/SlipTimeUi.cs
@@ -0,0 +1,32 @@
+﻿using SlipTime;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class SlipTimeUi : MonoBehaviour
+    {
+        public SlipTimeManager slipTimeManager;
+    
+        public Text slipTimeUiText;
+
+        private int slipTimeCharges;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            slipTimeCharges = slipTimeManager.slipTimeCharges;
+            slipTimeUiText.text = $"Slip Time Charges: {slipTimeCharges}";
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (slipTimeCharges != slipTimeManager.slipTimeCharges)
+            {
+                slipTimeCharges = slipTimeManager.slipTimeCharges;
+                slipTimeUiText.text = $"Slip Time Charges: {slipTimeCharges}";
+            }
+        }
+    }
+}
