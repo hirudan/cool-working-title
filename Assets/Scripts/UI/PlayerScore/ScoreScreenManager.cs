501e186e2431e8d3b172e23bea140f2c0f5973a5
hirudan <republiqueajamais@hotmail.com>
2020-11-14 00:56:54 -0800

    Implement final score screen

---
+++ Assets/Scripts/UI/PlayerScore/ScoreScreenManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI.PlayerScore
+{
+    public class ScoreScreenManager: MonoBehaviour
+    {
+        /// <summary>
+        /// The scene to load when the player clicks "Restart".
+        /// </summary>
+        public string gameScene;
+
+        /// <summary>
+        /// The scene to load when the player clicks "Main Menu"
+        /// </summary>
+        public string menuScene;
+        
+
+        public void OnRestartClicked()
+        {
+            SceneManager.LoadScene(gameScene);
+        }
+
+        public void OnMenuClicked()
+        {
+            SceneManager.LoadScene(menuScene);
+        }
+    }
+}
