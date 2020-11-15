1633f0564ea0ae52093ae1a900caa0302a5bd91a
Nathaniel Low <low.nathaniel@berkeley.edu>
2020-11-14 23:47:58 -0800

    Implement v2.0 of SlipTime
    Updated duration and cooldown lengths
    Bullet grazing decreases the cooldown
    Moving decreases the duration

--- Assets/Scripts/Actor/Shield.cs
+++ Assets/Scripts/Actor/Shield.cs
@@ -1,15 +1,17 @@
-﻿using System;
+﻿using SlipTime;
 using UnityEngine;
 
 namespace Actor
 {
     public class Shield : MonoBehaviour
     {
+        public SlipTimeManager slipTimeManager;
+        
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.gameObject.layer == 10)
             {
-                Debug.Log("Shields holding!");
+                slipTimeManager.DecreaseCooldown();
             }
         }
     }
