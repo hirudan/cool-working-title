814b19456fda4500ebf0590ea5964c31d458d295
hirudan <republiqueajamais@hotmail.com>
2020-11-15 22:29:47 -0800

    Implement first pass at boss

---
+++ Assets/Scripts/Actor/Boss.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using UnityEngine;
+
+namespace Actor
+{
+    public class Boss: MonoBehaviour
+    {
+        // A list of attack patterns that can contain non-spells and spells
+        public BossAttack[] attacks;
+
+        // How long to wait before switching to a new attack pattern
+        public float attackSwitchCooldownSeconds = 2f;
+
+        // The attack pattern currently active
+        private BossAttack currentAttack;
+
+        private int currentAttackIndex = 0;
+
+        private float timeElapsed = 0f;
+
+        // Damage taken during the current attack
+        private int damageTaken = 0;
+
+        private bool inAttack = false;
+
+        private Living vitality;
+
+        // The health at the beginning of the current attack
+        private int baselineHealth;
+
+        void Start()
+        {
+            currentAttack = attacks[0];
+            vitality = gameObject.GetComponent<Living>();
+            baselineHealth = vitality.Health;
+        }
+
+        private void Update()
+        {
+            // First, handle cooldowns between attacks
+            if (!inAttack && timeElapsed > attackSwitchCooldownSeconds)
+            {
+                timeElapsed = 0;
+                inAttack = true;
+                return;
+            }
+
+            try
+            {
+                if (timeElapsed > currentAttack.durationSeconds || damageTaken >= currentAttack.health)
+                {
+                    currentAttack.CleanUp();
+                    currentAttackIndex++;
+                    currentAttack = attacks[currentAttackIndex];
+                    timeElapsed = 0f;
+                    inAttack = false;
+                    baselineHealth = vitality.Health; // re-baseline health for next attack
+                }
+                else
+                {
+                    timeElapsed += Time.deltaTime;
+                    damageTaken = baselineHealth - vitality.Health;
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+                // If we catch an out of range exception, that means we finished the last attack
+                vitality.TakeDamage(vitality.Health);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Actor/BossAttack.cs
@@ -0,0 +1,52 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Actor
+{
+    public class BossAttack: MonoBehaviour
+    {
+        public float durationSeconds;
+
+        public int health;
+
+        [CanBeNull] public string name;
+        
+        /// <summary>
+        /// The score text box to update and display.
+        /// </summary>
+        public Text attackNameText;
+
+        public int scoreBonus = 0;
+
+        // A list of the emitters associated with this pattern
+        public GameObject[] emitterPrefabs;
+
+        // A list of the xy coordinates for each emitter
+        public int[] xCoords, yCoords;
+
+        void Start()
+        {
+            for (int index = 0; index < emitterPrefabs.Length; index++)
+            {
+                Instantiate(emitterPrefabs[index],
+                    new Vector3(xCoords[index], yCoords[index], 0), Quaternion.identity);
+            }
+
+            if (!string.IsNullOrEmpty(name))
+            {
+                attackNameText.text = name;
+            }
+        }
+
+        public void CleanUp()
+        {
+            foreach (var emitter in emitterPrefabs)
+            {
+                Destroy(emitter);
+            }
+
+            attackNameText.text = "";
+        }
+    }
+}
