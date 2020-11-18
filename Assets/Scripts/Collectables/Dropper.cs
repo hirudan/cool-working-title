169d4b69be048e5e1d976680a19045b26839e1b4
Nathaniel Low <low.nathaniel@berkeley.edu>
2020-11-18 00:12:11 -0800

    Add more XML comments

--- Assets/Scripts/Collectables/Dropper.cs
+++ Assets/Scripts/Collectables/Dropper.cs
@@ -2,8 +2,14 @@
 
 namespace Collectables
 {
+    /// <summary>
+    /// A generic dropper that drops collectables (drops).
+    /// </summary>
     public abstract class Dropper : MonoBehaviour
     {
+        /// <summary>
+        /// The collectable to drop.
+        /// </summary>
         public GameObject dropPrefab;
     }
 }
