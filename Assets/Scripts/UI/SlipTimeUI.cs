ea95b6f477fd4eb24c264b291f3a4dd1bbb7a48e
hirudan <republiqueajamais@hotmail.com>
2020-11-18 23:05:58 -0800

    Implement image-based sliptime UI

--- Assets/Scripts/UI/SlipTimeUI.cs
+++ Assets/Scripts/UI/SlipTimeUI.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using SlipTime;
+﻿using SlipTime;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,41 +8,53 @@ namespace UI
     {
         public SlipTimeManager slipTimeManager;
 
-        public Text slipTimeUIText;
-
         private int slipTimeCharges;
+
+        // Fillable images for the sliptime meter and charge bars
+        public Image meter;
+        public Image[] bars;
 
         // Start is called before the first frame update
         private void Start()
         {
+            meter.type = Image.Type.Filled;
+            meter.fillAmount = 1;
+            foreach (var img in bars)
+            {
+                img.type = Image.Type.Filled;
+                img.fillAmount = 1;
+            }
             slipTimeCharges = slipTimeManager.slipTimeCharges;
-            slipTimeUIText.text = $"Slip Time Charges: {slipTimeCharges}";
         }
 
         // Update is called once per frame
-        private void Update()
+        private void LateUpdate()
         {
-            var slipTimeText = $"Slip Time Charges: {slipTimeManager.slipTimeCharges}";
-            
-            // Leave this code here for now. We may be able to re-use it later once we get a better UI for SlipTime
-            // if we don't have to constantly update the text box.
-            // if (slipTimeCharges != slipTimeManager.slipTimeCharges)
-            // {
-            //     slipTimeCharges = slipTimeManager.slipTimeCharges;
-            //     slipTimeUIText.text = $"Slip Time Charges: {slipTimeCharges}";
-            // }
+            if (slipTimeCharges != slipTimeManager.slipTimeCharges)
+            {
+                slipTimeCharges = slipTimeManager.slipTimeCharges;
+                // Reset bars to unfilled
+                foreach (var img in bars)
+                {
+                    img.fillAmount = 0;
+                }
+                // Fill only the bars we need
+                for(var i = 0; i<slipTimeCharges; i++)
+                {
+                    bars[i].fillAmount = 1;
+                }
+            }
 
+            // Sliptime meter shild get emptied when sliptime is engaged
             if (slipTimeManager.InSlipTime)
             {
-                slipTimeText += $"\nSlip Time Duration: {Convert.ToInt32(slipTimeManager.SlipTimeCounter)}";
+                meter.fillAmount = 0;
             }
 
             if (slipTimeManager.IsChargingSlipTime)
             {
-                slipTimeText += $"\nSlip Time Cooldown: {Convert.ToInt32(slipTimeManager.ChargeTimeCounter)}";
+                meter.fillAmount = (slipTimeManager.slipTimeCooldownDuration - slipTimeManager.ChargeTimeCounter) / slipTimeManager.slipTimeCooldownDuration;
             }
-
-            slipTimeUIText.text = slipTimeText;
         }
     }
 }
