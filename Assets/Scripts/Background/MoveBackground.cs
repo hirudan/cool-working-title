af5c7a9603de88013cd6df74cc5a641d7a119107
ProgrammingIncluded <chencharlesh@gmail.com>
2020-11-20 01:20:35 -0800

    Add basic looping background into background test

---
+++ Assets/Scripts/Background/MoveBackground.cs
@@ -0,0 +1,46 @@
+﻿using SlipTime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBackground : MonoBehaviour, ISlipTimeAdherent
+{
+    public SlipTimeManager SlipTimeManager => slipTimeManager;
+
+    [SerializeField]
+    private SlipTimeManager slipTimeManager;
+
+
+    /// <summary>
+    /// Holds the point when the map loops and we should
+    /// stop translation and jump to the top of the map instead
+    /// Rather than to try to auto-guess this value, manually calculate it by hand
+    /// by moving the tilemap vertically until it matches.
+    /// Often than not, the number will probably work out nicely given
+    /// our camera coordinates being in round numbers.
+    /// </summary>
+    public float loopTranslationThreshold;
+
+    private Vector3 startLocation;
+
+    public float speed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startLocation = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((startLocation - transform.position).magnitude >= loopTranslationThreshold)
+        {
+            transform.position = startLocation;
+        }
+        else
+        {
+            transform.Translate(new Vector3(0, -speed * Time.deltaTime * SlipTimeManager.slipTimeCoefficient));
+        }
+    }
+}
