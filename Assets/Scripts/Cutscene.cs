cad27013777b66f4ef44f018d5e0962d9eb063ca
Charles H. Chen <chencharlesh@gmail.com>
2020-11-28 17:18:32 -0800

    Add back cutscene (#48)
    * Add back cutscene
    
    * Add pointer to cutscene

---
+++ Assets/Scripts/Cutscene.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Cutscene : MonoBehaviour
+{
+    public void LoadMainScene()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.timeSinceLevelLoad >= 1f)
+        {
+            if (Input.anyKeyDown)
+            {
+                LoadMainScene();
+            }
+        }
+    }
+}
